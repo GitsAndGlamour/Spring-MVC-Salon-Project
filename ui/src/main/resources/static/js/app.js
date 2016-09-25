753c25f742e2057cc81da3c878ac852daa044850
Omie Walls <omierwalls@gmail.com>
2016-09-24 20:08:37 -0400

    Implemented Navigation with Material Design and Angular Routing

--- ui/src/main/resources/static/js/app.js
+++ ui/src/main/resources/static/js/app.js
@@ -1,25 +1,36 @@
 // app.js
 var app = angular.module('app', ['ui.router', 'ngMaterial']);
 
-routerApp.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/');
 
     // PATH TO TEMPLATES =======================================================
-    var $path = '../../templates/';
+    var $path = '../templates/';
 
     $stateProvider
 
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/',
-            templateUrl: path+'home.html'
+            templateUrl: $path+'home.html'
         })
 
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
             url: '/about',
-            templateUrl: path+'about.html'
+            templateUrl: $path+'about.html'
         });
 
 });
+
+(function() {
+  'use strict';
+
+  angular.module('app', ['ngMaterial'])
+      .controller('NavCtrl', NavCtrl);
+
+  function NavCtrl($scope) {
+    $scope.currentNavItem = 'home';
+  }
+})();
